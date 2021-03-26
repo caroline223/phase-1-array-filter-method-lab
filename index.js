b892e3179cf3c92ba82953b3b66b84373d590b50
caroline223 <carolineforrester22@gmail.com>
2021-03-26 00:11:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { stringMatching } = require("expect");
+
 // Code your solution here
+
+
+function findMatching(drivers, str){
+    return drivers.filter(n => n.toLowerCase() === str.toLowerCase()
+)};
+
+function fuzzyMatch(drivers, str){
+    return drivers.filter(n => n.slice(0, str.length).toLowerCase() === str.toLowerCase())
+};
+
+function matchName(drivers, str){
+    return drivers.filter(n => n.name.toLowerCase() === str.toLowerCase());
+}
